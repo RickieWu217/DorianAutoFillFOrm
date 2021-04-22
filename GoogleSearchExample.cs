23292a47ff0160ee51e1c7c2d96293329be3f4bc
ADVANTECH\ED.Wu <ED.Wu@advantech.com.tw>
2021-04-22 08:48:34 +0800

    init

---
+++ GoogleSearchExample.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Newtonsoft.Json;
+
+namespace SeleniumExample
+{
+    [TestClass]
+    public class GoogleSearchExample
+    {
+        [TestMethod]
+        public void SearchForSeleniumHQ()
+        {
+            Assert.IsTrue(true);
+
+            return;
+
+            //Find folder with Chrome Driver (chromedriver.exe)
+            var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //Set Chrome to start with maximized window
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+
+            //Open Chrome
+            using (var chromeDriver = new ChromeDriver(browserDriverPath, options))
+            {
+                //Assign google.com to variable named url
+                var url = "http://google.com";
+                //Go to Google.com
+                chromeDriver.Navigate().GoToUrl(url);
+
+                //Create new wait timer and set it to 1 minute
+                var wait = new WebDriverWait(chromeDriver, new TimeSpan(0, 0, 1, 0));
+
+                //Wait until an element on the page with the name q is visible.
+                //Google named their search box q. probably short for query.
+                //We are waiting until that appears but will wait no longer than 1 minute as defined above.
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("q")));
+
+                //Find the Google Search Box now that it is visible and
+                //assign to the variable "googleSearchBox"
+                var googleSearchBox = chromeDriver.FindElement(By.Name("q"));
+
+                //clear search box just in case anything is already typed in
+                googleSearchBox.Clear();
+                //Type "Selenium HQ" into the search box
+                googleSearchBox.SendKeys("Selenium HQ");
+
+                //Wait until Google Search button is visible but don't wait more than 1 minute.
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name("btnK")));
+                //Find "Google Search" button and assign to variable name "searchButton"
+                var searchButton = chromeDriver.FindElement(By.Name("btnK"));
+
+                //Click search button
+                searchButton.Click();
+
+                //wait until search results stats appear which confirms that the search finished
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("resultStats")));
+
+                //Find result stats and assign to variable name resultStats
+                var resultStats = chromeDriver.FindElement(By.Id("resultStats"));
+
+                //Confirm the stats contain the words About, results and seconds.
+                //Example Result stats: "About 1,090,000 results (0.49 seconds)"
+                Assert.IsTrue(resultStats.Text.Contains("About"));
+                Assert.IsTrue(resultStats.Text.Contains("results"));
+                Assert.IsTrue(resultStats.Text.Contains("seconds"));
+
+                //Find a search result in the list
+                var results = chromeDriver.FindElement(By.ClassName("r"));
+
+                //Confirm that the result text contains Selenium
+                Assert.IsTrue(results.Text.Contains("Selenium"));
+
+                //close Chrome
+                chromeDriver.Close();
+            }
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            string fileName = @"C:\Users\ed.wu\Downloads\Selenium-C-Sharp-Example-master\SeleniumExample\Excel\Data.xlsx";
+            var datas = MyServices.ParseExcelDataToObject<ExcelDto>(fileName).ToList();
+
+            foreach (var item in datas)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            }
+
+            var browserDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //Set Chrome to start with maximized window
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+
+            //Open Chrome
+            using (var chromeDriver = new ChromeDriver(browserDriverPath, options))
+            {
+                var select = new SelectElement(chromeDriver.FindElementById(""));
+
+                //select.Options.Where(r => r.Text.Equals("")).FirstOrDefault().Selected;
+            }
+
+            Console.ReadLine();
+            Assert.IsTrue(true);
+
+        }
+
+    }
+
+
+   
+}
---
+++ Models.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumExample
+{
+    public class ExcelDto
+    {
+        [ColumnIndex(1)]
+        public string 合約起始日 { get; set; }
+        [ColumnIndex(2)]
+        public string 合約迄止日 { get; set; }
+        [ColumnIndex(3)]
+        public string 學校合約編號 { get; set; }
+        [ColumnIndex(4)]        
+        public string 是否公開 { get; set; }
+        [ColumnIndex(5)] 
+        public string 合約名稱 { get; set; }
+        [ColumnIndex(6)] 
+        public string 是否跨校 { get; set; }
+        [ColumnIndex(7)] 
+        public string 資金來源 { get; set; }
+        [ColumnIndex(8)] 
+        public string 合作模式 { get; set; }
+        [ColumnIndex(9)] 
+        public string 合約金額來源_來自政府 { get; set; }
+        [ColumnIndex(10)] 
+        public string 合約金額來源_來自企業 { get; set; }
+        [ColumnIndex(11)] 
+        public string 合約金額來源_來自其他單位 { get; set; }
+        [ColumnIndex(12)] 
+        public string 合約總金額 { get; set; }
+        [ColumnIndex(13)] 
+        public string 上傳合約書影本 { get; set; }
+        [ColumnIndex(14)] 
+        public string 本案分潤至學校聯盟_預定分潤金額 { get; set; }
+        [ColumnIndex(15)] 
+        public string 若分潤金額為0_請說明原因 { get; set; }
+        [ColumnIndex(16)] 
+        public string 平台協助項目_主要 { get; set; }
+        [ColumnIndex(17)] 
+        public string 平台協助項目_次要 { get; set; }
+        [ColumnIndex(18)] 
+        public string 請說明平台具體加值內容與協助項目 { get; set; }
+
+    }
+}
